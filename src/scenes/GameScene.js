0526554949208eecee81493767a22fef0ce0c830
Gunner <voragun2543@gmail.com>
2018-11-08 17:48:25 +0700

    [FIX]ground

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -45,8 +45,8 @@ class GameScene extends Phaser.Scene {
         platforms.create(400, 568, 'ground').setScale(2).refreshBody();
 
         //platforms.create(600, 400, 'ground');
-        platforms.create(50, 250, 'ground');
-        //platforms.create(750, 220, 'ground');
+        platforms.create(10, 250, 'ground');
+        platforms.create(400, 250, 'ground');
 
         
     }
