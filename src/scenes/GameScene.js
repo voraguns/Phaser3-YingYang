ba9e263c913eda79bb780afae47e29cb9b822e62
Duangporn Tangiam <dungporn.bt@gmail.com>
2018-11-11 16:10:38 +0700

    [ADD] Botton

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -7,6 +7,7 @@ let platforms;
 let player;
 let gameover = false;
 let cursors;
+let playimage1, playimage2, playimage3, playimage4, playimage5, playimage6;
 
 //import pic from '../../images/map';
 class GameScene extends Phaser.Scene {
@@ -27,6 +28,15 @@ class GameScene extends Phaser.Scene {
         this.load.image('lamp_off','../../images/map/lamp_off.png');
 
         this.load.spritesheet('yang','../../images/yang/ya1.png',{ frameWidth: 800, frameHeight: 600 } );
+
+        this.load.image('setting', '../../images/button/setting.png');
+        this.load.image('setting_point', '../../images/button/setting_point.png');
+        this.load.image('setting_page', '../../images/button/setting_page.png');
+        this.load.image('menu', '../../images/button/menu.png');
+        this.load.image('resume', '../../images/button/resume.png');
+        this.load.image('sound_on', '../../images/button/sound_on.png');
+        this.load.image('sound_off', '../../images/button/sound_off.png');
+        
 
         
     }
@@ -61,7 +71,30 @@ class GameScene extends Phaser.Scene {
         this.physics.add.collider(player, platforms);
         cursors = this.input.keyboard.createCursorKeys();
 
+        playimage1 = this.add.image(770, 30, 'setting');
+        playimage1.setInteractive();
+        playimage1.input.useHandCursor = true;
+        playimage1.on ('pointerup', () => { 
+            playimage2 = this.add.image(x, y, 'setting_page');
+            playimage2.setInteractive();
 
+            playimage3 = this.add.image(380,210, 'sound_on');
+            playimage3.setInteractive();
+
+            playimage4 = this.add.image(380,270, 'sound_off');
+            playimage4.setInteractive();
+
+            playimage5 = this.add.image(380,350, 'resume');
+            playimage5.setInteractive();
+            playimage5.on ('pointerup', () => {
+                this.input.on('gameobjectup',clickHandler, this);
+            });
+
+            playimage6 = this.add.image(380,410, 'menu');
+        });
+        
+        
+        
         
     }
 
@@ -85,7 +118,24 @@ class GameScene extends Phaser.Scene {
         if (cursors.up.isDown && player.body.touching.down){
         player.setVelocityY(-330);
     }
+
+    
   }
+}
+
+function hitFire(player, fire) {
+    player.setTint(0xff0000);
+    player.anims.play('turn');
+
+    gameover = true;
+}
+
+function clickHandler () {
+    playimage2.setVisible(false);
+    playimage3.setVisible(false);
+    playimage4.setVisible(false);
+    playimage5.setVisible(false);
+    playimage6.setVisible(false);
 }
 
 export default GameScene;
